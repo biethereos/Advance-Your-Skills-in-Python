69752a7e1ef9d796d7d0c26e002387289353e07e
HungNguyen <nguyenthanhhung@gmail.com>
2021-08-01 21:40:55 +0700

    end python algorithms

--- programmingFoundationsAlgorithms/6 Other Algorithms/findmax_start.py
+++ programmingFoundationsAlgorithms/6 Other Algorithms/findmax_start.py
@@ -6,14 +6,20 @@ items = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
 
 def find_max(items):
     # TODO: breaking condition: last item in list? return it
-
+    if len(items) == 1:
+        return items[0]
 
     # TODO: otherwise get the first item and call function
     # again to operate on the rest of the list
-
-
+    opt1 = items[0]
+    print(opt1)
+    opt2 = find_max(items[1:])
+    print(opt2)
     # TODO: perform the comparison when we're down to just two
-
+    if opt1 > opt2:
+        return opt1
+    else:
+        return opt2
 
 # test the function
 print(find_max(items))
