15e717892f801453c9254cce04b41719dc38e923
HungNguyen <nguyenthanhhung@gmail.com>
2021-08-11 16:09:14 +0700

    chap 2 finished python standard library

--- PythonStandardLibraryEssentialTraining/Chapter 3/03_01/filesbasic_start.py
+++ PythonStandardLibraryEssentialTraining/Chapter 3/03_01/filesbasic_start.py
@@ -3,7 +3,9 @@
 
 # TODO: open a file for writing data
 # "w" means write, "r" means read, "a" means append, "r+" means read and write
-
+fp = open('PythonStandardLibraryEssentialTraining/Chapter 3/03_01/testfile.txt', 'w')
+fp.write('This is sometext\n')
+fp.close()
 
 # TODO: read a file's data
 
