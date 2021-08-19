e97119c400696a92a20c36bddc05b8721149eb1c
HungNguyen <nguyenthanhhung@gmail.com>
2021-08-19 15:44:31 +0700

    update chap4 python standard library

--- PythonStandardLibraryEssentialTraining/Chapter 4/04_04/temppass_start.py
+++ PythonStandardLibraryEssentialTraining/Chapter 4/04_04/temppass_start.py
@@ -5,19 +5,27 @@ import string
 
 # TODO: Function to return a temporary password given a length
 def generateTempPass(numChars=8):
-    pass
-
+    potentialChars = string.ascii_letters + string.digits + '+=?/!@#$%*'
+    result = ''.join(secrets.choice(potentialChars) for i in range(numChars))
+    return result
 
 # TODO: Function to return a temporary password and enforce 1 number and 1 uppercase
 def generateBetterPass(numChars=8):
-    pass
-
+    potentialChars = string.ascii_letters + string.digits + '+=?/!@#$%*'
+    while True:
+        result = ''.join(secrets.choice(potentialChars) for i in range(numChars))
+        if(any(c.isupper() for c in result) and any(c.isdigit() for c in result)):
+            break
+        
+    return result
 # create a temporary password
 # print(generateTempPass(10))
 
 # create a stronger temporary password
-# print(generateBetterPass(10))
+print(generateBetterPass(10))
 
 
 # TODO: create a temporary, hard-to-guess URL
 resultUrl = "https://my.example.com?reset="
+resultUrl += secrets.token_urlsafe(15)
+print(resultUrl)
